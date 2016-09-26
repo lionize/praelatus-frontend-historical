178f88fe6bc1b8b92bf0587df05d1b95e067e50c
Mark Chandler <mark.allen.chandler@gmail.com>
2016-09-26 12:48:13 -0400

    Instal react-hot-loader and enable HMR

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,11 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: [
+    'webpack-dev-server/client?http://0.0.0.0:3000',
+    'webpack/hot/only-dev-server',
+    './src/index.js'
+  ],
 
   output: {
     filename: 'index.js',
@@ -22,5 +26,9 @@ module.exports = {
   resolve: {
     root: [path.resolve(__dirname, 'src'), path.resolve(__dirname, 'node_modules')],
     extensions: ['', '.js']
-  }
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 }
