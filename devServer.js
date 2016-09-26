0637a73ce18a4e163636c8892cdebc60ad63e890
Mark Chandler <mark.allen.chandler@gmail.com>
2016-09-26 12:01:08 -0400

    Create dev server

---
+++ devServer.js
@@ -0,0 +1,18 @@
+import path from 'path'
+import webpack from 'webpack'
+import webpackDevServer from 'webpack-dev-server'
+import config from './webpack.config'
+
+const host = '0.0.0.0'
+const port = 3000
+
+new webpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  historyApiFallback: true,
+}).list(port, host, (err) => {
+  if (err) {
+    return console.error(err)
+  }
+
+  console.log(`Listening at ${host}:${port}`)
+})
