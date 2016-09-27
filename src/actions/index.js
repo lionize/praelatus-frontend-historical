2fd390d9d3b747d935c995d869b695d931b70869
Mark Chandler <mark.allen.chandler@gmail.com>
2016-09-27 12:04:42 -0400

    Create actions for ticket fetching

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,20 +3,25 @@ import * as schema from 'actions/schema'
 import * as api from 'api'
 import * as types from 'constants'
 
+const fetchTicketsRequest = () => ({
+  type: types.FETCH_TICKETS_REQUEST
+})
+
+const fetchTicketsSuccess = (response) => ({
+  type: types.FETCH_TICKETS_SUCCESS,
+  response: normalize(response, schema.arrayOfTickets)
+})
+
+const fetchTicketsFailure = (error) => ({
+  type: types.FETCH_TICKETS_FAILURE,
+  message: error.message
+})
+
 export const fetchTickets = () => (dispatch) => {
+  dispatch(fetchTicketsRequest())
   return api.fetchTickets().then(
-    response => {
-      dispatch({
-        type: types.FETCH_TODOS_SUCCESS,
-        response: normalize(response, schema.arrayOfTickets)
-      })
-    },
-    error => {
-      dispatch({
-        type: types.FETCH_TODOS_FAILURE,
-        message: error.message || 'Something went wrong.'
-      })
-    }
+    response => dispatch(fetchTicketsSuccess(response)),
+    error => dispatch(fetchTicketsFailure(error))
   )
 }
 
