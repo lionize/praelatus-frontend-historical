1503144a71ca90359fc9c83afed543fba7215450
Mark Chandler <mark.allen.chandler@gmail.com>
2016-09-26 22:46:09 -0400

    Add more information to fake database

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,34 @@
 const fakeDatabase = {
-  tickets: [
-    {
-      id: 0,
-      summary: "This is just a summary",
-      description: "This description is much longer than the summary provided above."
-    }
-  ]
+	tickets: [
+		{
+			id: 0,
+      reporterId: 1,
+      assigneeId: 2,
+			summary: "This is just a summary",
+			description: "This description is much longer than the summary provided above."
+		}
+	],
+
+	users: [
+		{
+			"created_at": "Tue, 30 Aug 2016 21:07:53 GMT",
+			"email": "test@example.com",
+			"full_name": "Test Testerson II",
+			"id": 1,
+			"is_admin": null,
+			"updated_at": "Tue, 30 Aug 2016 21:07:53 GMT",
+			"username": "testadmin"
+		},
+		{
+			"created_at": "Tue, 30 Aug 2016 21:07:53 GMT",
+			"email": "test1@example.com",
+			"full_name": "Test Testerson",
+			"id": 2,
+			"is_admin": null,
+			"updated_at": "Tue, 30 Aug 2016 21:07:53 GMT",
+			"username": "test"
+		}
+	]
 }
 
 const delay = (ms) =>
