d43ff8da04a2d93a99305d453d7bf380dc00bce6
Mark Chandler <mark.allen.chandler@gmail.com>
2016-09-27 21:17:04 -0400

    Wrote tests for more ticket actions

--- test/actions/tickets_spec.js
+++ test/actions/tickets_spec.js
@@ -5,45 +5,106 @@ import * as types from 'constants/actionTypes'
 import nock from 'nock'
 import { expect } from 'chai'
 
+import {
+  FETCH_TICKETS_SUCCESS,
+  FETCH_TICKETS_FAILURE,
+  FETCH_TICKETS_REQUEST,
+} from 'constants/actionTypes'
+
+import {
+  fetchTicketsRequest,
+  fetchTicketsFailure,
+  fetchTicketsSuccess,
+} from 'actions'
+
 const middlewares = [thunk]
 const mockStore = configureMockStore(middlewares)
 
 const URL = 'http://localhost:8080/api/v1'
 
-describe('ticket actions', () => {
+describe('Ticket Actions', () => {
   afterEach(() => {
     nock.cleanAll()
   })
 
-  it('creates FETCH_TICKETS_SUCCESS when fetching tickets is finished', () => {
-    nock(URL)
-      .get('/tickets')
-      .reply(200, { body: [{
+  describe('fetchTicketsRequest', () => {
+    it('should return the correct type', () => {
+      const expectedResult = {
+        type: FETCH_TICKETS_REQUEST,
+      }
+
+      expect(fetchTicketsRequest()).to.deep.eq(expectedResult)
+    })
+  })
+
+  describe('fetchTicketsSuccess', () => {
+    it('should return the correct type and the correct response', () => {
+      const fixture = [{
         id: 1,
         description: 'Ticket description',
-        summary: 'Ticket summary'
-      }]})
-
-    const expectedActions = [
-      { type: types.FETCH_TICKETS_REQUEST },
-      { type: types.FETCH_TICKETS_SUCCESS, response: {
-        entities: {
-          tickets: {
-            1: {
-              id: 1,
-              description: "Ticket description",
-              summary: "Ticket summary"
+        summary: 'Ticket summary',
+      }]
+      const expectedResult = {
+        type: FETCH_TICKETS_SUCCESS,
+        response: {
+          result: [1],
+          entities: {
+            tickets: {
+              1: fixture[0]
             }
           }
-        },
-        result: [1]
-      }}
-    ]
-    const store = mockStore({ tickets: [] })
+        }
+      }
 
-    store.dispatch(actions.fetchTickets())
-      .then(() => {
-        expect(store.getActions()).to.eq(expectedActions)
-      })
+      expect(fetchTicketsSuccess(fixture)).to.deep.eq(expectedResult)
+    })
+  })
+
+  describe('fetchTicketsFailure', () => {
+    it('should return the correct type and the error', () => {
+      const fixture = {
+        message: 'Error!'
+      }
+      const expectedResult = {
+        type: FETCH_TICKETS_FAILURE,
+        message: fixture.message
+      }
+
+      expect(fetchTicketsFailure(fixture)).to.deep.eq(expectedResult)
+    })
+  })
+
+  describe('fetchTickets', () => {
+    it('creates FETCH_TICKETS_SUCCESS when fetching tickets is finished', () => {
+      nock(URL)
+        .get('/tickets')
+        .reply(200, { body: [{
+          id: 1,
+          description: 'Ticket description',
+          summary: 'Ticket summary'
+        }]})
+
+      const expectedActions = [
+        { type: types.FETCH_TICKETS_REQUEST },
+        { type: types.FETCH_TICKETS_SUCCESS, response: {
+          entities: {
+            tickets: {
+              1: {
+                id: 1,
+                description: "Ticket description",
+                summary: "Ticket summary"
+              }
+            }
+          },
+          result: [1]
+        }}
+      ]
+      const store = mockStore({ tickets: [] })
+
+      store.dispatch(actions.fetchTickets())
+        .then(() => {
+          expect(store.getActions()).to.eq(expectedActions)
+        })
+    })
   })
 })
