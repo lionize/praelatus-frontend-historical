f7ca7feaed945578bfe23bd225c25a183dc384fb
Mark Chandler <mark.allen.chandler@gmail.com>
2016-10-05 19:31:56 -0400

    Dispatch fetchTickets when mounting ConnectedTicket

--- src/containers/ConnectedTicket.js
+++ src/containers/ConnectedTicket.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
 
+import { fetchTickets } from 'actions/tickets'
 import { ticketSelector } from 'selectors/tickets'
 
 import Ticket from 'components/Ticket'
 
 class ConnectedTicket extends Component {
+  componentWillMount() {
+    this.props.dispatch(fetchTickets())
+  }
+
   render() {
     return (
       <Ticket {...this.props} />
