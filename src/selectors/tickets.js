cf2d6ff5013998611cd98349d4d53ba38e392789
Mark Chandler <mark.allen.chandler@gmail.com>
2016-10-04 20:16:17 -0400

    Create single ticket selector

--- src/selectors/tickets.js
+++ src/selectors/tickets.js
@@ -2,3 +2,7 @@ export const ticketsSelector = (state) => {
   const ids = state.tickets.ids
   return ids.map(id => state.tickets.byId[id])
 }
+
+export const ticketSelector = (state, id) => {
+  return state.tickets.byId[id]
+}
