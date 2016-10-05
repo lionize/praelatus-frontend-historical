61acab571538aa0ea46d20997f1faa8d478bb2c0
Mark Chandler <mark.allen.chandler@gmail.com>
2016-10-04 20:18:18 -0400

    Add ConnectedTicket route

--- src/components/Root.js
+++ src/components/Root.js
@@ -3,14 +3,14 @@ import { Provider } from 'react-redux'
 import { Router, Route, browserHistory } from 'react-router'
 import App from 'components/App'
 import TicketsPage from 'containers/TicketsPage'
-import Ticket from 'components/Ticket'
+import ConnectedTicket from 'containers/ConnectedTicket'
 
 const Root = ({ store }) => (
   <Provider store={store}>
     <Router history={browserHistory}>
       <Route path='/' component={App}>
         <Route path="tickets" component={TicketsPage} />
-        <Route path="tickets/:id" component={Ticket} />
+        <Route path="tickets/:id" component={ConnectedTicket} />
       </Route>
     </Router>
   </Provider>
