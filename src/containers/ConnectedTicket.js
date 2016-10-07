7f085965c1280d3046f9d445f7d2acc143ca604b
Mark Chandler <mark.allen.chandler@gmail.com>
2016-10-07 15:26:57 -0400

    Start work on loading state for Ticket component

--- src/containers/ConnectedTicket.js
+++ src/containers/ConnectedTicket.js
@@ -3,25 +3,37 @@ import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
 
 import { fetchTickets } from 'actions/tickets'
-import { ticketSelector } from 'selectors/tickets'
+import { ticketSelector, loadingSelector } from 'selectors/tickets'
 
 import Ticket from 'components/Ticket'
 
 class ConnectedTicket extends Component {
+  static defaultProps = {
+    loading: true
+  }
+
   componentWillMount() {
     this.props.dispatch(fetchTickets())
   }
 
   render() {
+    console.log(this.props.loading)
+    if (this.props.loading) {
+      return (
+        <span>Loading...</span>
+      )
+    }
+
     return (
-      <Ticket {...this.props} />
+      <Ticket {...this.props.ticket} />
     )
   }
 }
 
 const mapStateToProps = (state, { params }) => {
   const ticket = ticketSelector(state, params.id)
-  return { ...ticket }
+  const loading = loadingSelector(state)
+  return { ticket, loading }
 }
 
 ConnectedTicket = withRouter(connect(
