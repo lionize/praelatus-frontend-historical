921f27d45e91af7c8cfa04e68972faed41bf76c5
Mark Chandler <mark.allen.chandler@gmail.com>
2016-10-07 16:33:55 -0400

    Convert tickets to ducks module

--- src/selectors/tickets.js
+++
@@ -1,8 +0,0 @@
-export const ticketsSelector = (state) => {
-  const ids = state.tickets.ids
-  return ids.map(id => state.tickets.byId[id])
-}
-
-export const ticketSelector = (state, id) => state.tickets.byId[id]
-
-export const loadingSelector = (state) => state.tickets.loading
