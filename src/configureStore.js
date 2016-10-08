05538c731c3fdc8a84fd911a9cc74cbdb79df84f
Mark Chandler <mark.allen.chandler@gmail.com>
2016-10-08 16:38:17 -0400

    Fix reducers import in configureStore

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import createLogger from 'redux-logger'
-import reducers from 'reducers'
+import reducers from 'modules'
 
 const configureStore = () => {
   const middlewares = [thunk]
