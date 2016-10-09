63597203bae6e05f00e02972e4934f406c663d09
Mark Chandler <mark.allen.chandler@gmail.com>
2016-10-09 19:07:10 -0400

    Add basic styling to Header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -3,7 +3,7 @@ import styles from './header.css'
 
 const Header = () => (
   <div className={styles.container}>
-    <h1 className={styles.brand}>Tessera</h1>
+    <h1 className={styles.brand}>T</h1>
   </div>
 )
 
