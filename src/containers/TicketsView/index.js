548c25d5cba9b43115566b5d4ada7558ff876759
Mark Chandler <mark.allen.chandler@gmail.com>
2016-10-09 19:42:18 -0400

    Connect TicketView to state

--- src/containers/TicketsView/index.js
+++ src/containers/TicketsView/index.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import { actions } from 'modules/tickets'
+import { ticketsSelector } from 'modules/tickets'
+
+import TicketList from 'components/TicketList'
 import styles from './ticketsView.css'
 
 class TicketsView extends Component {
+  componentWillMount() {
+    this.props.fetchTickets()
+  }
+
   render() {
+    const { tickets } = this.props
+
     return (
-      <div className={styles.container}>
-        Hi! This is the TicketsView
-      </div>
+      <TicketList tickets={tickets} />
     )
   }
 }
 
-export default TicketsView
+const mapStateToProps = (state) => ({
+  tickets: ticketsSelector(state)
+})
+
+export default connect(mapStateToProps, actions)(TicketsView)
