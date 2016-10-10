54b58bac9387f6230bf2c72f9cfeb76d1bdbc5cd
Mark Chandler <mark.allen.chandler@gmail.com>
2016-10-10 18:47:51 -0400

    Create basic TicketDisplay component

---
+++ src/containers/TicketDisplay.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class TicketDisplay extends Component {
+  componentWillMount() {
+  }
+
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+export default TicketDisplay
