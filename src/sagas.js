52fa11c263232c3fefcb1d5c1ecd20a3d2590c8c
Mark Chandler <mark.allen.chandler@gmail.com>
2016-10-17 00:26:44 -0400

    Import ticket actions into sagas

--- src/sagas.js
+++ src/sagas.js
@@ -1,14 +1,14 @@
 import { takeEvery, takeLatest } from 'redux-saga'
 import { call, put } from 'redux-saga/effects'
-import { types } from 'modules/tickets'
+import { actions, types } from 'modules/tickets'
 import api from 'api'
 
-function* fetchTickets(action) {
+export function* fetchTickets(action) {
   try {
-    const tickets = yield call(api.fetchTickets, action.payload)
-    yield put(types.fetchTicketsSuccess(tickets))
+    const response = yield call(api.fetchTickets)
+    yield put(actions.fetchTicketsSuccess(response.body))
   } catch (e) {
-    yield put(types.fetchTicketsFailure(e))
+    yield put(actions.fetchTicketsFailure(e))
   }
 }
 
