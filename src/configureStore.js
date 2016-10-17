a1012141457877e279a98ee56420f65d78f1c023
Mark Chandler <mark.allen.chandler@gmail.com>
2016-10-17 00:04:34 -0400

    Initialize redux-saga middleware

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,18 +1,24 @@
 import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
 import createLogger from 'redux-logger'
 import reducers from 'modules'
 
+const sagaMiddleware = createSagaMiddleware()
+
 const configureStore = () => {
-  const middlewares = []
+  const middlewares = [sagaMiddleware]
 
   if (process.env.NODE_ENV !== 'production') {
     middlewares.push(createLogger())
   }
 
-  return createStore(
-    reducers,
-    applyMiddleware(...middlewares)
-  )
+  return {
+    ...createStore(
+      reducers,
+      applyMiddleware(...middlewares),
+    ),
+    runSaga: sagaMiddleware.run,
+  }
 }
 
 export default configureStore
