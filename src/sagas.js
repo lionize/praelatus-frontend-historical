514cedaf6e06396b8175b1a963a90d775059c16f
Mark Chandler <mark.allen.chandler@gmail.com>
2016-10-20 21:07:21 -0400

    Document fetchTickets saga

--- src/sagas.js
+++ src/sagas.js
@@ -1,8 +1,21 @@
+/** @module sagas */
+
 import { takeEvery, takeLatest } from 'redux-saga'
 import { call, put } from 'redux-saga/effects'
 import { actions, types } from 'modules/tickets'
 import api from 'api'
 
+/**
+ * Saga that handles the API call for fetching tickets.
+ *
+ * The saga takes an action and passes the payload field to the server API's fetchTickets method. This will attempt to request tickets from the server.
+ *
+ * If the request is a success, the fetchTicketsSuccess action is dispatched along with the response.
+ *
+ * If the request is a failure, the fetchTicketsFailure action is dispatched along with the error.
+ *
+ * @param {object} action - The dispatched action which the saga is responding to.
+ */
 export function* fetchTickets(action) {
   try {
     const response = yield call(api.fetchTickets, {})
