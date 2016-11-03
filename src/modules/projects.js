12f9a7220bf19f1b7fb39e0a52c9d4b998f65f82
Mark Chandler <mark.allen.chandler@gmail.com>
2016-11-02 20:56:09 -0400

    Create Project index file

---
+++ src/modules/projects.js
@@ -0,0 +1,4 @@
+export { default as actions } from 'modules/projects/actions'
+export { default as types } from 'modules/projects/types'
+export * from 'modules/projects/selectors'
+export { default as default } from 'modules/projects/reducers'
