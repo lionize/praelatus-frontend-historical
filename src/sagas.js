676248be8131868bc8a2a7d99b34cdd257acedb5
Mark Chandler <mark.allen.chandler@gmail.com>
2016-11-14 18:41:04 -0500

    Create Ticket module sagas

--- src/sagas.js
+++ src/sagas.js
@@ -1,19 +1,4 @@
-import { takeEvery, takeLatest } from 'redux-saga'
-import { call, put } from 'redux-saga/effects'
-import { actions, types } from 'modules/tickets'
-import api from 'api'
+import sagasManager from 'sagasManager'
+import { ticketSagas } from 'modules/tickets'
 
-export function* fetchTickets(action) {
-  try {
-    const response = yield call(api.fetchTickets, {})
-    yield put(actions.fetchTicketsSuccess(response))
-  } catch (e) {
-    yield put(actions.fetchTicketsFailure(e))
-  }
-}
-
-function* mySaga() {
-  yield* takeEvery(types.FETCH_TICKETS_REQUEST, fetchTickets)
-}
-
-export default mySaga
+sagasManager.addSagaToRoot(ticketSagas)
