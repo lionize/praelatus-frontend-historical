be81e945dc973c2c0be64ba9b34d776dccebfaae
Mark Chandler <mark.allen.chandler@gmail.com>
2016-11-27 18:39:45 -0500

    Create all ticket sagas

--- src/modules/tickets/sagas.js
+++ src/modules/tickets/sagas.js
@@ -3,17 +3,51 @@ import { call, put } from 'redux-saga/effects'
 import { actions, types } from 'modules/tickets'
 import api from 'api'
 
-function* fetchTickets(action) {
+export function* fetchTickets(action = {}) {
   try {
-    const response = yield call(api.fetchTickets, {})
+    const payload = action.payload || {}
+    const response = yield call(api.fetchTickets, payload)
     yield put(actions.fetchTicketsSuccess(response))
   } catch (e) {
     yield put(actions.fetchTicketsFailure(e))
   }
 }
 
+export function* createTicket(action = {}) {
+  try {
+    const payload = action.payload || {}
+    const response = yield call(api.createTicket, payload)
+    yield put(actions.createTicketSuccess(response))
+  } catch (e) {
+    yield put(actions.createTicketFailure(e))
+  }
+}
+
+export function* updateTicket(action = {}) {
+  try {
+    const payload = action.payload || {}
+    const response = yield call(api.updateTicket, payload)
+    yield put(actions.updateTicketSuccess(response))
+  } catch (e) {
+    yield put(actions.updateTicketFailure(e))
+  }
+}
+
+export function* deleteTicket(action = {}) {
+  try {
+    const payload = action.payload || {}
+    const response = yield call(api.deleteTicket, payload)
+    yield put(actions.deleteTicketSuccess(response))
+  } catch (e) {
+    yield put(actions.deleteTicketFailure(e))
+  }
+}
+
 export default function* watcher() {
   yield [
     takeEvery(types.FETCH_TICKETS_REQUEST, fetchTickets),
+    takeEvery(types.CREATE_TICKET_REQUEST, createTicket),
+    takeEvery(types.UPDATE_TICKET_REQUEST, updateTicket),
+    takeEvery(types.DELETE_TICKET_REQUEST, deleteTicket),
   ]
 }
