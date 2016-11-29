e556a8fc619ee22fa54a3e6b0ce29de7ad89e783
Mark Chandler <mark.allen.chandler@gmail.com>
2016-11-29 14:21:13 -0500

    Create users redux module with tests

---
+++ src/modules/users/selectors.js
@@ -0,0 +1,49 @@
+/** @module users/selectors */
+
+/**
+ * Selector that fetches all users from the user state.
+ *
+ * The selector collects all of the ids from users.ids and then maps them into
+ * the corresponding users.
+ *
+ * @function
+ * @param {Map} state - The global state.
+ * @returns {List} - A List of selected users.
+ */
+export const usersSelector = (state) => {
+  const userIds = state.getIn(['users', 'ids'])
+  return userIds.map(id => userSelector(state, id))
+}
+
+/**
+ * Selector that fetches a single user from the user state.
+ *
+ * The selector gets all of the users by id from users.byId and then gets
+ * the user by the passed id from that List.
+ *
+ * @function
+ * @param {Map} state - The global state.
+ * @param {number|string} id - The id of the user being selected.
+ * @returns {Map} - The selected user.
+ */
+export const userSelector = (state, id) => {
+  return state.getIn(['users', 'byId']).get(String(id))
+}
+
+/**
+ * Selector that fetches the loading state from the user state.
+ *
+ * @function
+ * @param {Map} state - The global state.
+ * @returns {boolean} - The current loading state.
+ */
+export const loadingSelector = state => state.getIn(['users', 'loading'])
+
+/**
+ * Selector that fetches the error message from the user state.
+ *
+ * @function
+ * @param {Map} state - The global state.
+ * @returns {string?} - The current error message.
+ */
+export const errorSelector = state => state.getIn(['users', 'error'])
---
+++ test/modules/users/sagas_spec.js
@@ -0,0 +1,122 @@
+import { expect } from 'chai'
+import { put, call } from 'redux-saga/effects'
+import api from 'api'
+import { actions } from 'modules/users'
+import * as sagas from 'modules/users/sagas'
+
+describe('users module sagas', () => {
+  describe('GET: fetch users', () => {
+    it('calls the api method', () => {
+      const generator = sagas.fetchUsers()
+
+      expect(generator.next().value).to.deep.eq(call(api.fetchUsers, {}))
+    })
+
+    it('fetches tickets', () => {
+      const generator = sagas.fetchUsers()
+      generator.next()
+      const response = []
+      const next = generator.next(response).value
+      const expected = put(actions.fetchUsersSuccess(response))
+
+      expect(next.PUT.action.type).to.eq(expected.PUT.action.type)
+      expect(next.PUT.action.response).to.eq(expected.PUT.action.response)
+    })
+
+    it('returns an error if fetching fails', () => {
+      const generator = sagas.fetchUsers()
+      generator.next()
+      const response = { message: 'Error!' }
+
+      expect(generator.throw(response).value).to.deep.eq(put(actions.fetchUsersFailure(response)))
+    })
+  })
+
+  describe('POST: create user', () => {
+    const fixture = {
+      name: 'The A User',
+    }
+
+    it('calls the api method', () => {
+      const generator = sagas.createUser({ payload: fixture })
+
+      expect(generator.next().value).to.deep.eq(call(api.createUser, fixture))
+    })
+
+    it('creates a user', () => {
+      const generator = sagas.createUser({ payload: fixture })
+      generator.next()
+      const response = {
+        id: 0,
+        ...fixture
+      }
+      const next = generator.next(response).value
+      const expected = put(actions.createUserSuccess(response))
+
+      expect(next.PUT.action.type).to.equal(expected.PUT.action.type)
+      expect(next.PUT.action.response).to.equal(expected.PUT.action.response)
+    })
+
+    it('returns an error if creation fails', () => {
+      const generator = sagas.createUser({ payload: fixture })
+      generator.next()
+      const response = { message: 'Error!' }
+
+      expect(generator.throw(response).value).to.deep.eq(put(actions.createUserFailure(response)))
+    })
+  })
+
+  describe('PUT: update user', () => {
+    const fixture = {
+      icon: "",
+      name: 'The A User',
+      urlSlug: 'the-a-user'
+    }
+
+    it('calls the api method', () => {
+      const generator = sagas.updateUser({ payload: fixture })
+
+      expect(generator.next().value).to.deep.eq(call(api.updateUser, fixture))
+    })
+
+    it('updates the ticket', () => {
+      const generator = sagas.updateUser({ payload: fixture })
+      generator.next()
+      const response = fixture
+      const next = generator.next(response).value
+      const expected = put(actions.updateUserSuccess(response))
+
+      expect(next.PUT.action.type).to.equal(expected.PUT.action.type)
+      expect(next.PUT.action.response).to.equal(expected.PUT.action.response)
+    })
+
+    it('generates an error if updating fails', () => {
+      const generator = sagas.updateUser({ payload: fixture })
+      generator.next()
+      const response = { message: 'Error!' }
+
+      expect(generator.throw(response).value).to.deep.eq(put(actions.updateUserFailure(response)))
+    })
+  })
+
+  describe('DELETE: delete user', () => {
+    const fixture = { id: 0 }
+
+    it('calls the api method', () => {
+      const generator = sagas.deleteUser({ payload: fixture })
+
+      expect(generator.next().value).to.deep.eq(call(api.deleteUser, fixture))
+    })
+
+    it('deletes a user', () => {
+      const generator = sagas.deleteUser({ payload: fixture })
+      generator.next()
+      const response = fixture
+      const next = generator.next(response).value
+      const expected = put(actions.deleteUserSuccess(response))
+
+      expect(next.PUT.action.type).to.equal(expected.PUT.action.type)
+      expect(next.PUT.action.id).to.equal(expected.PUT.action.id)
+    })
+  })
+})
