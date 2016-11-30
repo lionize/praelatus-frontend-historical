2772029aae99e47b8d1f96d421c09afbd29842c1
Mark Chandler <mark.allen.chandler@gmail.com>
2016-11-30 14:52:11 -0500

    Add all module sagas to sagasManager

--- src/modules/projects.js
+++ src/modules/projects.js
@@ -1,5 +1,8 @@
 export { default as actions } from 'modules/projects/actions'
 export { default as types } from 'modules/projects/types'
-export { default as sagas } from 'modules/projects/sagas'
 export * from 'modules/projects/selectors'
 export { default as default } from 'modules/projects/reducers'
+
+import { default as sagas } from 'modules/projects/sagas'
+import sagasManager from 'sagasManager'
+sagasManager.addSagaToRoot(sagas)
--- src/modules/teams.js
+++ src/modules/teams.js
@@ -1,5 +1,8 @@
 export { default as actions } from 'modules/teams/actions'
 export { default as types } from 'modules/teams/types'
-export { default as sagas } from 'modules/teams/sagas'
 export * from 'modules/teams/selectors'
 export { default as default } from 'modules/teams/reducers'
+
+import { default as sagas } from 'modules/teams/sagas'
+import sagasManager from 'sagasManager'
+sagasManager.addSagaToRoot(sagas)
