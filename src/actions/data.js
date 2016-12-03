65f3aede354975aa97950ec0675b8a32a74052dd
Mark Chandler <mark.allen.chandler@gmail.com>
2016-12-02 19:29:15 -0500

    Update fetchDataSuccess to always expect array

--- src/actions/data.js
+++ src/actions/data.js
@@ -2,19 +2,8 @@ import { normalize, arrayOf } from 'normalizr-immutable'
 import * as schema from 'schema'
 import types from 'types/data'
 
-export const fetchDataSuccess = (response, type, singular = false) => {
-  let data
-
-  if (singular) {
-    data = normalize(response, schema[type], {})
-  } else {
-    data = normalize(response, arrayOf(schema[type]), {})
-  }
-
-  return {
-    type: types.FETCH_DATA_SUCCESS,
-    response: data,
-    responseType: type,
-    singular,
-  }
-}
+export const fetchDataSuccess = (response, type) => ({
+  type: types.FETCH_DATA_SUCCESS,
+  response: normalize(response, arrayOf(schema[type])),
+  responseType: type,
+})
