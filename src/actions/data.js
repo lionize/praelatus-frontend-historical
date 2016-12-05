3ed05479435489da0b9ad0bf5cccfde339e1bd5d
Mark Chandler <mark.allen.chandler@gmail.com>
2016-12-04 22:36:04 -0500

    Create formatResponse actions
    formatResponse takes normalized data and alters the information. It
    returns the entities key of the normalized response without changing it
    but recreates the result key.
    
    The result key is a Map whose keys are the different types of entities
    and whose values are lists of the ids of those entities.

--- src/actions/data.js
+++ src/actions/data.js
@@ -1,9 +1,18 @@
 import { normalize, arrayOf } from 'normalizr-immutable'
+import { Map, List } from 'immutable'
 import * as schema from 'schema'
 import types from 'types/data'
 
 export const fetchDataSuccess = (response, type) => ({
   type: types.FETCH_DATA_SUCCESS,
-  response: normalize(response, arrayOf(schema[type])),
-  responseType: type,
+  response: formatResponse(normalize(response, arrayOf(schema[type]))),
 })
+
+export const formatResponse = data => {
+  return Map({
+    entities: data.get('entities'),
+    result: data.get('entities')._keys.reduce((acc, key) => {
+      return acc.set(key, List(data.getIn(['entities', key])._keys.map(key => parseInt(key))))
+    }, Map({}))
+  })
+}
--- test/actions/data_spec.js
+++ test/actions/data_spec.js
@@ -1,86 +1,90 @@
 import { expect } from 'chai'
+import { normalize, arrayOf } from 'normalizr-immutable'
+import * as schema from 'schema'
 import types from 'types/data'
 import * as actions from 'actions/data'
 
 describe('data actions', () => {
   describe('fetchDataSuccess', () => {
-    describe('singular object response', () => {
-      const fixture = {
+    const fixture = [
+      {
         id: 0,
         name: 'A Team',
+      },
+      {
+        id: 1,
+        name: 'B Team',
       }
-      const expectedResult = {
-        type: types.FETCH_DATA_SUCCESS,
-        singular: true,
-        responseType: 'team',
-        response: {
-          result: 0,
-          entities: {
-            teams: {
-              0: fixture
+    ]
+    const expectedResult = {
+      type: types.FETCH_DATA_SUCCESS,
+      response: {
+        entities: {
+          teams: {
+            0: {
+              id: 0,
+              name: 'A Team'
+            },
+            1: {
+              id: 1,
+              name: 'B Team'
             }
           }
+        },
+        result: {
+          teams: [0, 1]
         }
       }
-      const result = actions.fetchDataSuccess(fixture, 'team', true)
+    }
+    const result = actions.fetchDataSuccess(fixture, 'team')
 
-      it('should return the correct type', () => {
-        expect(result.type).to.eq(expectedResult.type)
-      })
+    it('should return the correct type', () => {
+      expect(result.type).to.eq(expectedResult.type)
+    })
 
-      it('should return the correct response', () => {
-        expect(result.response.toJS()).to.deep.eq(expectedResult.response)
-      })
+    it('should return the correct response', () => {
+      expect(result.response.get('entities').toJS()).to.deep.eq(expectedResult.response.entities)
+      expect(result.response.get('result').toJS()).to.deep.eq(expectedResult.response.result)
+    })
+  })
 
-      it('should return the correct response type', () => {
-        expect(result.responseType).to.eq(expectedResult.responseType)
-      })
+  describe('formatResponse', () => {
+    const author = {
+      id: 1,
+      username: 'mark',
+      email: 'mark@test.com',
+      fullName: 'Mark',
+      gravatar: 'mark@test.com',
+      profilePic: 'mark@test.com',
+      isAdmin: false,
+    }
+    const comment = [{
+      id: 2,
+      body: 'A Comment',
+      author: author,
+    }]
+    const normalized = normalize(comment, arrayOf(schema.comment))
+    const result = actions.formatResponse(normalized)
 
-      it('should return the correct singular flag', () => {
-        expect(result.singular).to.eq(expectedResult.singular)
+    it('should return the correct entities', () => {
+      expect(result.get('entities').toJS()).to.deep.eq({
+        users: {
+          '1': author,
+        },
+        comments: {
+          '2': {
+            id: 2,
+            body: 'A Comment',
+            author: author.id,
+          }
+        },
       })
     })
-    describe('multiple object response', () => {
-      const fixture = [
-        {
-          id: 0,
-          name: 'A Team',
-        },
-        {
-          id: 1,
-          name: 'B Team',
-        }
-      ]
-      const expectedResult = {
-        type: types.FETCH_DATA_SUCCESS,
-        singular: false,
-        responseType: 'team',
-        response: {
-          result: [0, 1],
-          entities: {
-            teams: {
-              0: fixture[0],
-              1: fixture[1]
-            }
-          }
-        }
-      }
-      const result = actions.fetchDataSuccess(fixture, 'team')
 
-      it('should return the correct type', () => {
-        expect(result.type).to.eq(expectedResult.type)
-      })
-
-      it('should return the correct response', () => {
-        expect(result.response.toJS()).to.deep.eq(expectedResult.response)
-      })
-
-      it('should return the correct response type', () => {
-        expect(result.responseType).to.eq(expectedResult.responseType)
-      })
-
-      it('should return the correct singular flag', () => {
-        expect(result.singular).to.eq(false)
+    it('should return the correct result', () => {
+      expect(result.get('result').toJS()).to.deep.eq({
+        users: [1],
+        comments: [2],
       })
     })
   })
