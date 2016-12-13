872a85dd224e808adcb98439f05a8223f65ce8ad
Mark Chandler <mark.allen.chandler@gmail.com>
2016-12-12 19:54:13 -0500

    Remove old containers and components

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import React from 'react'
 import { Route } from 'react-router'
-
 import App from 'components/App'
-import TicketsView from 'containers/TicketsView'
-import TicketsSidebar from 'containers/TicketsSidebar'
-import TicketDisplay from 'containers/TicketDisplay'
 
 const routes = (
   <Route path='/' component={App}>
-    <Route path="tickets" components={{ main: TicketsView, sidebar: TicketsSidebar }}>
-      <Route path=":id" component={TicketDisplay} />
-    </Route>
   </Route>
 )
 
