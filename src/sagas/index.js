fb24cbf3b1188cbb997a325ffa9e42547a00972a
Mark Chandler <mark.allen.chandler@gmail.com>
2016-12-16 18:20:32 -0500

    Reconfigure sagas
    - Created an index file for sagas that imports all sagas and exports
    them as an array
    - Imported sagas array in index and add each to SagasManager

---
+++ src/sagas/index.js
@@ -0,0 +1,13 @@
+import tickets from 'sagas/tickets'
+import teams from 'sagas/teams'
+import projects from 'sagas/projects'
+import users from 'sagas/users'
+import comments from 'sagas/comments'
+
+export default [ 
+  tickets,
+  teams,
+  projects,
+  users,
+  comments,
+]
--- src/sagasManager.js
+++ src/sagasManager.js
@@ -19,3 +19,4 @@ class SagasManager {
 }
 
 export default new SagasManager
+
