fa56f01d05382454b8b76b4c0bd23ccc98080d28
Mark Chandler <mark.allen.chandler@gmail.com>
2016-12-16 22:29:39 -0500

    Use Gravatar component in UserList

--- src/components/UserList/index.js
+++ src/components/UserList/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Table } from 'reactstrap'
+import Gravatar from 'components/Gravatar'
+import './userList.css'
 
 const UserList = ({ users }) => {
   return (
@@ -15,7 +17,12 @@ const UserList = ({ users }) => {
         <tbody>
           {users.map(user =>
             <tr key={user.id}>
-              <td>Gravatar</td>
+              <td>
+                <Gravatar
+                  email={user.email}
+                  size={30}
+                />
+              </td>
               <td>{user.username}</td>
               <td>{user.fullName}</td>
             </tr>
