00b49cc0f080c7b070fda01a30a48ec6eaf87f98
Mark Chandler <mark.allen.chandler@gmail.com>
2016-12-18 23:18:51 -0500

    Add link to /projects to Header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router'
 import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap'
 import './Header.css'
 
@@ -14,7 +15,7 @@ const Header = () => (
         <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">Link</a>
+        <Link to="/projects" className="nav-link">Projects</Link>
       </li>
       <li className="nav-item">
         <a className="nav-link" href="#">Link</a>
