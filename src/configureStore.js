0662e6e54afd7c289b5dd47467d150161f17e945
Mark Chandler <mark.allen.chandler@gmail.com>
2016-12-18 21:46:36 -0500

    Add transformer to redux-logger so action output is in JS objects

--- src/configureStore.js
+++ src/configureStore.js
@@ -11,7 +11,17 @@ const configureStore = () => {
 
   if (process.env.NODE_ENV !== 'production') {
     const logger = createLogger({
-        stateTransformer: state => state && state.toJS()
+      stateTransformer: state => state && state.toJS(),
+      actionTransformer: action => {
+        if (action && action.response) {
+          return {
+            ...action,
+            response: action.response.toJS()
+          }
+        } else {
+          return action
+        }
+      }
     })
     middlewares.push(logger)
   }
