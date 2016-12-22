604eb48e9c91bd7723a26377c5b1884a6f131f12
Mark Chandler <mark.allen.chandler@gmail.com>
2016-12-22 09:37:27 -0500

    Remove denormalization from TicketContainer

--- src/containers/tickets/TicketContainer.js
+++ src/containers/tickets/TicketContainer.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react'
 import { withRouter } from 'react-router'
 import { connect } from 'react-redux'
 import { ticketSelector } from 'selectors/tickets'
-import { userSelector } from 'selectors/users'
 import { fetchTicketsRequest } from 'actions/tickets'
-import { fetchUsersRequest } from 'actions/users'
 import { Ticket } from 'components/tickets'
 
 class TicketContainer extends Component {
@@ -25,13 +23,9 @@ class TicketContainer extends Component {
 
 const mapStateToProps = (state, { params }) => {
   const ticket = ticketSelector(state, params.id)
-  const reporter = ticket ? userSelector(state, ticket.get('reporter')) : null
-  const assignee = ticket ? userSelector(state, ticket.get('assignee')) : null
 
   return {
     ticket,
-    reporter,
-    assignee,
   }
 }
 
