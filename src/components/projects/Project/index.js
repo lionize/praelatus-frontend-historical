77a5553d9549d4ec08cd8a357bfbf97e7e1d07b5
Mark Chandler <mark.allen.chandler@gmail.com>
2016-12-22 09:39:07 -0500

    Add UserLink to Project

--- src/components/projects/Project/index.js
+++ src/components/projects/Project/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Button, Card, CardBlock, CardTitle, CardText } from 'reactstrap'
 import { Link } from 'react-router'
+import { UserLink } from 'components/misc'
 import { NotFoundCard, ErrorCard } from 'components/cards'
 
-const Project = ({ project, lead, error }) => {
+const Project = ({ project, error }) => {
   if (project) {
     return (
       <div>
@@ -12,8 +13,10 @@ const Project = ({ project, lead, error }) => {
             <CardTitle>{project.name}</CardTitle>
             <CardText>key: {project.key}</CardText>
             <CardText>homepage: {project.homepage}</CardText>
-            {lead &&
-              <CardText>Lead: <Link to={`/users/${lead.id}`}>{lead.username}</Link></CardText>
+            {project.lead &&
+              <CardText>
+                Lead: <UserLink id={project.lead.id}>{project.lead.username}</UserLink>
+              </CardText>
             }
           </CardBlock>
         </Card>
