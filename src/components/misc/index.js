e8bcacd812956c55de19040c0eebfaee97e711ce
Mark Chandler <mark.allen.chandler@gmail.com>
2016-12-22 09:32:39 -0500

    Create TicketLink component

--- src/components/misc/index.js
+++ src/components/misc/index.js
@@ -1,3 +1,6 @@
 export { default as LinkButton } from 'components/misc/LinkButton'
+
 export { default as Gravatar } from 'components/misc/Gravatar'
+
 export { default as UserLink } from 'components/misc/UserLink'
+export { default as TicketLink } from 'components/misc/TicketLink'
