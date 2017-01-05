a881faa29aa8065a8c1c6183083fb0c4635d3c09
Mark Chandler <mark.allen.chandler@gmail.com>
2017-01-05 16:40:57 -0500

    Remove link components index

--- src/components/links/index.js
+++
@@ -1,4 +0,0 @@
-export { default as UserLink } from 'components/links/UserLink'
-export { default as TicketLink } from 'components/links/TicketLink'
-export { default as ProjectLink } from 'components/links/ProjectLink'
-export { default as TeamLink } from 'components/links/TeamLink'
