c97f438a4b6683cd9bb61b41779683d06fe47cf9
Mark Chandler <mark.allen.chandler@gmail.com>
2017-01-13 19:51:14 -0500

    Add authReducer to index reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux-immutablejs'
 import { reducer as formReducer } from 'redux-form/immutable'
 import dataReducers from 'reducers/data'
 import routerReducer from 'reducers/router'
+import authReducer from 'reducers/auth'
 
 export default combineReducers({
   data: dataReducers,
   form: formReducer,
   routing: routerReducer,
+  auth: authReducer,
 })
