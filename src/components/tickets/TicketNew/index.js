7786126d0640e645343cef6dd4cd90a1c9e48109
Mark Chandler <mark.allen.chandler@gmail.com>
2017-01-16 17:56:13 -0500

    Remove TicketNew component

--- src/components/tickets/TicketNew/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { TicketForm } from 'components'
-
-const TicketNew = ({ handleSubmit }) => (
-  <TicketForm handleSubmit={handleSubmit} />
-)
-
-export default TicketNew
