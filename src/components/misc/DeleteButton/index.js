3500981b308fcdeb61546da45691c90cb7cf6225
Mark Chandler <mark.allen.chandler@gmail.com>
2017-01-16 21:07:28 -0500

    Create DeleteButton component

---
+++ src/components/misc/DeleteButton/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Button } from 'components'
+
+const DeleteButton = ({ handleClick }) => (
+  <Button onClick={handleClick}>Delete</Button>
+)
+
+export default DeleteButton
