23e76a52c7c7fca2e7d229671a307f46ba2006b7
Mark Chandler <mark.allen.chandler@gmail.com>
2017-01-17 19:29:45 -0500

    Create TeamTable component

---
+++ src/components/teams/TeamTable/index.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Table } from 'reactstrap'
+import { TeamLink, UserLink } from 'components'
+
+const TeamTable = ({ teams }) => (
+  <div>
+    <Table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Lead</th>
+          <th>Members</th>
+        </tr>
+      </thead>
+      <tbody>
+        {teams.map((team, i) =>
+          <tr key={i}>
+            <td>
+              <TeamLink id={team.id}>{team.name}</TeamLink>
+            </td>
+            <td>
+              {team.lead &&
+                <UserLink id={team.lead.id}>{team.lead.username}</UserLink>
+              }
+            </td>
+            <td>
+              {team.members &&
+                <ul>
+                  {team.members.map((member, j) =>
+                    <li key={j}>
+                      <UserLink id={member.id}>{member.username}</UserLink>
+                    </li>
+                  )}
+                </ul>
+              }
+            </td>
+          </tr>
+        )}
+      </tbody>
+    </Table>
+  </div>
+)
+
+export default TeamTable
