ba22729a385ff9bc2dc8869ef0b716ad57e22019
Mark Chandler <mark.allen.chandler@gmail.com>
2017-01-17 19:45:09 -0500

    Create TicketTable component

---
+++ src/components/tickets/TicketTable/index.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Table } from 'reactstrap'
+import { TicketLink, UserLink } from 'components'
+
+const TicketTable = ({ tickets }) => (
+  <div>
+    <Table>
+      <thead>
+        <tr>
+          <th>id</th>
+          <th>Key</th>
+          <th>Summary</th>
+          <th>Description</th>
+          <th>Reporter</th>
+          <th>Assignee</th>
+        </tr>
+      </thead>
+      <tbody>
+        {tickets.map((ticket, i) =>
+          <tr key={i}>
+            <td>{ticket.id}</td>
+            <td>
+              <TicketLink id={ticket.id}>{ticket.key}</TicketLink>
+            </td>
+            <td>{ticket.summary}</td>
+            <td>{ticket.description}</td>
+            <td>
+              {ticket.reporter &&
+                <UserLink id={ticket.reporter.id}>{ticket.reporter.username}</UserLink>
+              }
+            </td>
+            <td>
+              {ticket.assignee &&
+                <UserLink id={ticket.assignee.id}>{ticket.assignee.username}</UserLink>
+              }
+            </td>
+          </tr>
+        )}
+      </tbody>
+    </Table>
+  </div>
+)
+
+export default TicketTable
