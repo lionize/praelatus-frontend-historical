c3a7ceffee2910e47426a65327df990c3768151e
Mark Chandler <mark.allen.chandler@gmail.com>
2017-01-17 19:19:20 -0500

    Create ProjectTable component

---
+++ src/components/projects/ProjectTable/index.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Table } from 'reactstrap'
+import { ProjectLink, UserLink } from 'components'
+
+const ProjectTable = ({ projects }) => (
+  <div>
+    <Table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Key</th>
+          <th>Lead</th>
+          <th>Created Date</th>
+          <th>Homepage</th>
+          <th>Repo</th>
+        </tr>
+      </thead>
+      <tbody>
+        {projects.map((project, i) =>
+          <tr key={i}>
+            <td>
+              <ProjectLink id={project.id}>{project.name}</ProjectLink>
+            </td>
+            <td>
+              <ProjectLink id={project.id}>{project.key}</ProjectLink>
+            </td>
+            <td>
+              {project.lead &&
+                <UserLink id={project.lead.id}>{project.lead.username}</UserLink>
+              }
+            </td>
+            <td>{project.createdDate}</td>
+            <td>{project.homepage}</td>
+            <td>{project.repo}</td>
+          </tr>
+        )}
+      </tbody>
+    </Table>
+  </div>
+)
+
+export default ProjectTable
