c47c04fb08ee59e16e2f4707b290f7bf1fb3dcd8
Mark Chandler <mark.allen.chandler@gmail.com>
2017-02-12 20:40:43 -0500

    Create comment Redux module

---
+++ src/modules/commentRedux.js
@@ -0,0 +1,95 @@
+import { createReducer, createActions } from 'reduxsauce'
+import { mergeWith } from 'ramda'
+import Immutable from 'seamless-immutable'
+import deepMerge from 'util-deep-merge'
+
+/* TYPES AND ACTION CREATORS */
+
+const { Types: types, Creators: creators } = createActions({
+  fetchRequest: ['payload'],
+  fetchFailure: ['error'],
+  createRequest: ['payload'],
+  createSuccess: ['comment'],
+  createFailure: ['error'],
+  updateRequest: ['payload'],
+  updateSuccess: ['comment'],
+  updateFailure: ['error'],
+  deleteRequest: ['id'],
+  deleteSuccess: ['id'],
+  deleteFailure: ['error'],
+});
+
+export const commentTypes = types
+export default creators
+
+/* INITIAL STATE */
+
+export const INITIAL_STATE = Immutable({
+  byId: {},
+  ids: [],
+  error: null,
+  fetching: false,
+})
+
+/* REDUCERS */
+
+export const request = state =>
+  state.merge({
+    fetching: true,
+    error: null,
+  })
+
+export const success = (state, { comment }) =>
+  mergeWith(deepMerge, state, {
+    fetching: false,
+    error: null,
+    ids: [comment.id],
+    byId: { [comment.id]: comment }
+  })
+
+export const failure = (state, { error }) => state.merge({ fetching: false, error })
+
+export const remove = (state, { id }) => state.merge({
+  fetching: false,
+  error: null,
+  ids: state.ids.filter(i => i !== id),
+  byId: state.byId.without(id)
+})
+
+
+/* HOOKUP REDUCERS TO TYPES */
+
+export const reducer = createReducer(INITIAL_STATE, {
+  [types.FETCH_REQUEST]: request,
+  [types.FETCH_FAILURE]: failure,
+
+  [types.CREATE_REQUEST]: request,
+  [types.CREATE_SUCCESS]: success,
+  [types.CREATE_FAILURE]: failure,
+
+  [types.UPDATE_REQUEST]: request,
+  [types.UPDATE_SUCCESS]: success,
+  [types.UPDATE_FAILURE]: failure,
+
+  [types.DELETE_REQUEST]: request,
+  [types.DELETE_SUCCESS]: remove,
+  [types.DELETE_FAILURE]: failure,
+})
+
+/* SELECTORS */
+
+export const comment = (state, id) => state.byId[String(id)]
+
+export const comments = (state, ids) => {
+  let commentIds = state.ids
+
+  if (ids) {
+    commentIds = commentIds.filter(i => ids.includes(i))
+  }
+
+  return commentIds.map(i => comment(state, i))
+}
+
+export const fetching = state => state.fetching
+
+export const error = state => state.error
---
+++ test/modules/comment_spec.js
@@ -0,0 +1,93 @@
+import { expect } from 'chai'
+import actions, {
+  reducer,
+  INITIAL_STATE,
+  comment,
+  comments,
+  fetching,
+  error,
+} from 'modules/commentRedux'
+
+describe('Comment - Redux', () => {
+  describe('reducers', () => {
+    it('request', () => {
+      const startingState = INITIAL_STATE.merge({
+        error: 'Error',
+      })
+      const state = reducer(startingState, actions.fetchRequest())
+
+      expect(state.fetching).to.be.true
+      expect(state.error).to.be.null
+    })
+
+    it('success', () => {
+      const comment = {
+        id: 0,
+        body: 'A comment!'
+      }
+      const state = reducer(INITIAL_STATE, actions.updateSuccess(comment))
+
+      expect(state.fetching).to.be.false
+      expect(state.error).to.be.null
+      expect(state.ids).to.include(0)
+      expect(state.byId['0']).to.eq(comment)
+    })
+
+    it('failure', () => {
+      const state = reducer(INITIAL_STATE, actions.updateFailure('Error'))
+
+      expect(state.fetching).to.be.false
+      expect(state.error).to.eq('Error')
+    })
+
+    it('remove', () => {
+      const startingState = INITIAL_STATE.merge({
+        ids: [0],
+        byKey: { '0': { body: 'Comment' } },
+        fetching: true,
+        error: 'Error',
+      })
+      const state = reducer(startingState, actions.deleteSuccess(0))
+
+      expect(state.error).to.be.null
+      expect(state.fetching).to.be.false
+      expect(state.ids).to.not.include(0)
+      expect(state.byId).to.not.have.key('0')
+    })
+  })
+
+  describe('selectors', () => {
+    const state = {
+      ids: [0, 1],
+      byId: {
+        '0': {
+          id: 0,
+          body: 'comment-0',
+        },
+        '1': {
+          id: 1,
+          body: 'comment-1'
+        },
+      },
+      fetching: true,
+      error: 'Error',
+    }
+
+    it('comment', () => {
+      expect(comment(state, 0)).to.eq(state.byId['0'])
+    })
+
+    it('comments', () => {
+      const expected = [state.byId['0'], state.byId['1']]
+      expect(comments(state, [0, 1])).to.deep.eq(expected)
+    })
+
+    it('fetching', () => {
+      expect(fetching(state)).to.be.true
+    })
+
+    it('error', () => {
+      expect(error(state)).to.eq('Error')
+    })
+  })
+})
