ebd3cf2a1bd7a8db34f79bdc1f89681979ef81cc
Mark Chandler <mark.allen.chandler@gmail.com>
2017-02-13 22:53:57 -0500

    Add saga middleware to store creation

--- src/modules/createStore.js
+++ src/modules/createStore.js
@@ -1,13 +1,21 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import createLogger from 'redux-logger'
+import createSagaMiddleware from 'redux-saga'
 
 const DEV = process.env.NODE_ENV !== 'production'
 
-export default rootReducer => {
+export default (rootReducer, rootSaga) => {
   /* REDUX CONFIGURATION */
 
   const middleware = []
+  const enhancers = []
 
+  /* SAGA MIDDLEWARE */
+
+  const sagaMiddleware = createSagaMiddleware()
+  middleware.push(sagaMiddleware)
+
+  /* LOGGER MIDDLEWARE */
   if (DEV) {
     const logger = createLogger()
     middleware.push(logger)
@@ -15,7 +23,11 @@ export default rootReducer => {
 
   /* ASSEMBLE MIDDLEWARE */
 
-  const store = createStore(rootReducer, compose(applyMiddleware(...middleware)))
+  enhancers.push(applyMiddleware(...middleware))
+
+  const store = createStore(rootReducer, compose(...enhancers))
+
+  sagaMiddleware.run(rootSaga)
 
   return store
 }
