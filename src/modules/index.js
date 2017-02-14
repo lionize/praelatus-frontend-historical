6c820589375cb7fd8467d332d1e41706e565e149
Mark Chandler <mark.allen.chandler@gmail.com>
2017-02-13 22:10:27 -0500

    Add Redux modules to root reducer

--- src/modules/index.js
+++ src/modules/index.js
@@ -3,7 +3,14 @@ import configureStore from 'modules/createStore'
 
 export default () => {
   const rootReducer = combineReducers({
-    tickets: require('./ticketRedux').reducer,
+    data: combineReducers({
+      tickets: require('./ticketRedux').reducer,
+      projects: require('./projectRedux').reducer,
+      teams: require('./teamRedux').reducer,
+      comments: require('./commentRedux').reducer,
+      users: require('./userRedux').reducer,
+    }),
+    auth: require('./authRedux').reducer,
   })
 
   return configureStore(rootReducer)
