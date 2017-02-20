abcd4aec6c9dc21e4bdc480ef1478cb6100c353d
Mark Chandler <mark.allen.chandler@gmail.com>
2017-02-20 18:34:11 -0500

    Remove unused dataRedux module

--- src/modules/dataRedux.js
+++
@@ -1,8 +0,0 @@
-import { createActions } from 'reduxsauce'
-
-const { Types: types, Creators: creators } = createActions({
-  fetchSuccess: ['response'],
-})
-
-export const dataTypes = types
-export default creators
