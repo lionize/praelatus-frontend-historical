bb83d35fce57416260c1a409e3d87274956fc53d
Mark Chandler <mark.allen.chandler@gmail.com>
2017-02-21 20:28:06 +0000

    Create root saga with saga and types imports

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,15 +1,31 @@
-import tickets from 'sagas/tickets'
-import teams from 'sagas/teams'
-import projects from 'sagas/projects'
-import users from 'sagas/users'
-import comments from 'sagas/comments'
-import auth from 'sagas/auth'
+import { takeLatest, takeEvery } from 'redux-saga'
+import api from 'services/api'
 
-export default [
-  tickets,
-  teams,
-  projects,
-  users,
-  comments,
-  auth,
-]
+/* Types */
+import { authTypes } from 'modules/authRedux'
+import { commentTypes } from 'modules/commentRedux'
+import { projectTypes } from 'modules/projectRedux'
+import { teamTypes } from 'modules/teamRedux'
+import { ticketTypes } from 'modules/ticketRedux'
+import { userTypes } from 'modules/userRedux'
+
+/* Sagas */
+
+import {
+  login, register, logout
+} from 'sagas/auth'
+import {
+  fetchComment, fetchComments, createComment, updateComment, deleteComment
+} from 'sagas/comments'
+import {
+  fetchProject, fetchProjects, createProject, updateProject, deleteProject
+} from 'sagas/projects'
+import {
+  fetchTeam, fetchTeams, createTeam, updateTeam, deleteTeam
+} from 'sagas/teams'
+import {
+  fetchTicket, fetchTickets, createTicket, updateTicket, deleteTicket
+} from 'sagas/tickets'
+import {
+  fetchUser, fetchUsers, createUser, updateUser, deleteUser
+} from 'sagas/users'
