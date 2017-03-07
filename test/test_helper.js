e15da446367132b1270cffa64bb4ce7f1106b35e
Mark Chandler <mark.allen.chandler@gmail.com>
2017-03-07 13:18:33 +0000

    Remove chaiImmutable import

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,8 +1,5 @@
 import chai from 'chai'
-import chaiImmutable from 'chai-immutable'
 import jsdom from 'jsdom';
-
-chai.use(chaiImmutable)
 
 const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
 const win = doc.defaultView;
