e42929ffd4dd7a0130f4a33597dd3e5efa69fb9b
Mark Chandler <mark.allen.chandler@gmail.com>
2017-03-10 18:34:46 -0500

    Combine LoginForm and Login components

--- src/components/auth/Login/index.js
+++ src/components/auth/Login/index.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import autobind from 'autobind-decorator'
+import { Field, reduxForm } from 'redux-form'
 import actions from 'modules/auth'
-import { LoginForm } from 'components'
+import { renderField } from 'utils'
+import { Form, Button } from 'components'
+
+let LoginForm = ({ handleSubmit }) => (
+  <Form onSubmit={handleSubmit}>
+    <Field name="username" component={renderField} type="text" label="Username" />
+    <Field name="password" component={renderField} type="password" label="Password" />
+    <Button>Submit</Button>
+  </Form>
+)
+
+LoginForm = reduxForm({
+  form: 'login',
+})(LoginForm)
 
 class Login extends Component {
   @autobind
