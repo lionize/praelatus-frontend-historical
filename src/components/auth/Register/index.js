6ce88209ae5e1f4c31c3f9a609d7634696134338
Mark Chandler <mark.allen.chandler@gmail.com>
2017-03-10 18:33:22 -0500

    Combine RegisterForm and Register components

--- src/components/auth/Register/index.js
+++ src/components/auth/Register/index.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import autobind from 'autobind-decorator'
+import { Field, reduxForm } from 'redux-form'
+import { renderField } from 'utils'
 import actions from 'modules/auth'
-import { RegisterForm } from 'components'
+import { Form, Button } from 'components'
+
+let RegisterForm = ({ handleSubmit }) => (
+  <Form onSubmit={handleSubmit}>
+    <Field name="username" component={renderField} type="text" label="Username" />
+    <Field name="password" component={renderField} type="password" label="Password" />
+    <Field name="name" component={renderField} type="text" label="Name" />
+    <Field name="email" component={renderField} type="email" label="Email" />
+    <Button>Submit</Button>
+  </Form>
+)
+
+RegisterForm = reduxForm({
+  form: 'register',
+})(RegisterForm)
 
 class Register extends Component {
   @autobind
