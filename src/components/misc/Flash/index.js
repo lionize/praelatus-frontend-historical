8904284af6c7d14212b359883d1cf795e4f20409
Mark Chandler <mark.allen.chandler@gmail.com>
2017-03-10 14:28:37 -0500

    Remove console.log from Flash

--- src/components/misc/Flash/index.js
+++ src/components/misc/Flash/index.js
@@ -1,8 +1,6 @@
 import React from 'react'
 
 const Flash = ({ message }) => {
-  console.log('message', message)
-
   if(message == null || message.length == 0 ||
      (typeof(message) == 'object' && message.data == null)) {
     return (<div></div>)
@@ -10,14 +8,14 @@ const Flash = ({ message }) => {
 
   if(typeof(message) == 'object') {
     return (
-        <div className="flash-message">
+      <div className="flash-message">
         {message.data.message}
       </div>
     )
   }
 
   return (
-      <div className="flash-message">
+    <div className="flash-message">
       {message}
     </div>
   )
