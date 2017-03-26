35babdc4841a981e7e9d0c6f95c0edac18f2de34
Mark Chandler <mark.allen.chandler@gmail.com>
2017-03-26 18:35:38 +0000

    Fix FormFeedback export

--- src/components/forms/index.js
+++ src/components/forms/index.js
@@ -1,5 +1,5 @@
 export { default as Form } from 'components/forms/Form'
-export { default as FormFeedbackFeedback } from 'components/forms/FormFeedback'
+export { default as FormFeedback } from 'components/forms/FormFeedback'
 export { default as FormGroup } from 'components/forms/FormGroup'
 export { default as FormText } from 'components/forms/FormText'
 export { default as Input } from 'components/forms/Input'
