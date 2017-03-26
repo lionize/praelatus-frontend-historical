eedff7360d8cf2891bd4e29870df36dc3fbc262e
Mark Chandler <mark.allen.chandler@gmail.com>
2017-03-26 16:27:22 -0400

    Test Flash

---
+++ test/components/Flash.spec.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { expect } from 'chai'
+import { shallow } from 'enzyme'
+import { Flash } from 'components'
+
+describe('Flash Component', () => {
+  it('renders', () => {
+    const wrapper = shallow(<Flash message={''} />)
+
+    expect(wrapper.exists()).to.be.true
+  })
+
+  it('can take an object', () => {
+    const wrapper = shallow(<Flash message={{ data: { message: 'Message' } }} />)
+
+    expect(wrapper).to.contain.text('Message')
+  })
+
+  it('can take a string', () => {
+    const wrapper = shallow(<Flash message={'Message'} />)
+
+    expect(wrapper).to.contain.text('Message')
+  })
+
+  it('renders an empty div if no message', () => {
+    const wrapper = shallow(<Flash />)
+
+    expect(wrapper.text().length).to.eq(0)
+  })
+})
