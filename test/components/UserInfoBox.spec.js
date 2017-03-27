85653f8fd4bfc6cc1af8166bc04e37975f69adbf
Mark Chandler <mark.allen.chandler@gmail.com>
2017-03-27 18:28:24 +0000

    Test UserInfoBox

---
+++ test/components/UserInfoBox.spec.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { expect } from 'chai'
+import { shallow, mount } from 'enzyme'
+import sinon from 'sinon'
+import { wrapWithProvider } from '../utilities'
+import BoxContainer, { UserInfoBox as BoxComponent } from 'components/auth/UserInfoBox'
+import { ProfileBox, LoginLink, RegisterLink } from 'components'
+
+describe('UserInfoBox Component', () => {
+  it('renders', () => {
+    const state = {
+      auth: {
+        currentUser: {}
+      }
+    }
+    const wrapper = mount(
+      wrapWithProvider(BoxContainer, { state })
+    )
+
+    const container = wrapper.find(BoxContainer)
+    const component = wrapper.find(BoxComponent)
+
+    expect(container.exists()).to.be.true
+    expect(component.exists()).to.be.true
+  })
+
+  context('when logged in', () => {
+    it('displays a ProfileBox', () => {
+      const wrapper = shallow(<BoxComponent loggedIn={true} user={{ username: 'user0' }} />)
+      const profileBox = wrapper.find(ProfileBox)
+
+      expect(profileBox.exists()).to.be.true
+      expect(profileBox.prop('user')).to.deep.eq({ username: 'user0' })
+    })
+  })
+
+  context('when logged out', () => {
+    it('displays a LoginLink and RegisterLink', () => {
+      const wrapper = shallow(<BoxComponent />)
+
+      expect(wrapper.find(LoginLink).exists()).to.be.true
+      expect(wrapper.find(RegisterLink).exists()).to.be.true
+    })
+  })
+})
