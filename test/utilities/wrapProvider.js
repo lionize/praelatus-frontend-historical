10f2bd39af7fb26fc76052e618c6d75acf79eca6
Mark Chandler <mark.allen.chandler@gmail.com>
2017-03-28 18:48:45 +0000

    Rewrite wrapProvider as HOC

---
+++ test/utilities/wrapProvider.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import configureStore from 'redux-mock-store'
+import { Provider } from 'react-redux'
+
+export default function wrapProvider({ state }) {
+  const store = configureStore()(state ? state : {})
+
+  return function WrapProviderFactory(WrappedComponent) {
+    return class WrapProvider extends React.Component {
+      render() {
+        return (
+          <Provider store={store}>
+            <WrappedComponent {...this.props} />
+          </Provider>
+        )
+      }
+    }
+  }
+}
