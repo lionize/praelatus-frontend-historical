74e1131d20fffd54a33f190db6b152565f00bfdf
Mark Chandler <mark.allen.chandler@gmail.com>
2017-04-01 15:45:18 -0400

    Disallow semicolons in eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,7 +35,7 @@ module.exports = {
   },
 
   rules: {
-    semi: 0,
+    semi: ['error', 'never'],
     'jsx-quotes': 0,
     'no-undef': 0,
     'arrow-body-style': 0,
@@ -52,8 +52,6 @@ module.exports = {
     'import/prefer-default-export': 0,
 
     'react/jsx-filename-extension': 0,
-    'react/forbid-prop-types': 0,
-    'react/prop-types': 0,
 
     'jsx-a11y/href-no-hash': 0,
   }
