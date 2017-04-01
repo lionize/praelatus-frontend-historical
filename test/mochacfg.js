a7387f9bf168eb2240d822d4864a144fcc0e38f5
Mark Chandler <mark.allen.chandler@gmail.com>
2017-04-01 15:00:10 -0400

    Document mochacfg.js

--- test/mochacfg.js
+++ test/mochacfg.js
@@ -1,1 +1,6 @@
+/*
+  Configuration file for .png extensions. Allows mocha to understand how to
+  handle the file format in component tests.
+*/
+
 require.extensions['.png'] = function mochaPngConfig() { return null; }
