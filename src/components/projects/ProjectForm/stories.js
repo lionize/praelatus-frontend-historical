b8179b7d26fa4bcc986deace0bd58fc2291286e1
Mark Chandler <mark.allen.chandler@gmail.com>
2017-04-11 19:35:18 -0400

    Add Provider to ProjectForm

--- src/components/projects/ProjectForm/stories.js
+++ src/components/projects/ProjectForm/stories.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { storiesOf } from '@kadira/storybook'
+import Provider from 'provider'
 import { ProjectForm } from 'components'
 
 storiesOf('ProjectForm', module)
+  .addDecorator(Provider)
   .add('renders', () => (
-    <ProjectForm>Render</ProjectForm>
+    <ProjectForm />
   ))
