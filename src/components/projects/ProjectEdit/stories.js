effa2949ec63fe6d8fcfb7b02d89b76204592db2
Mark Chandler <mark.allen.chandler@gmail.com>
2017-04-11 19:41:46 -0400

    Remove ProjectEdit stories

--- src/components/projects/ProjectEdit/stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@kadira/storybook'
-import { ProjectEdit } from 'components'
-
-storiesOf('Form', module)
-  .add('renders', () => (
-    <ProjectEdit>Render</ProjectEdit>
-  ))
