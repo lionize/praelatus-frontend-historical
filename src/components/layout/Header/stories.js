53a160bc626f9e5b8b321c15089b0b8e877522f9
Mark Chandler <mark.allen.chandler@gmail.com>
2017-04-12 17:00:35 -0400

    Add Provider to Header

--- src/components/layout/Header/stories.js
+++ src/components/layout/Header/stories.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import { storiesOf } from '@kadira/storybook'
+import Provider from 'provider'
 import { Header } from 'components'
 
+const store = {
+  auth: {
+    currentUser: null,
+  }
+}
+
 storiesOf('Header', module)
+  .addDecorator(story => (
+    Provider(story, store)
+  ))
   .add('renders', () => (
     <Header>Render</Header>
   ))
