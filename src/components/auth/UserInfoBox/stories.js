ff10735e45a78596fa0330b3c4f1a59deaae1beb
Mark Chandler <mark.allen.chandler@gmail.com>
2017-04-12 16:31:57 -0400

    Add Provider to UserInfoBox

--- src/components/auth/UserInfoBox/stories.js
+++ src/components/auth/UserInfoBox/stories.js
@@ -1,8 +1,20 @@
 import React from 'react'
 import { storiesOf } from '@kadira/storybook'
+import Provider from 'provider'
 import { UserInfoBox } from 'components'
 
+const store = {
+  auth: {
+    currentUser: {
+      username: 'mark',
+    }
+  }
+}
+
 storiesOf('UserInfoBox', module)
+  .addDecorator(story => (
+    Provider(story, store)
+  ))
   .add('renders', () => (
     <UserInfoBox>Render</UserInfoBox>
   ))
