5d9f96806f42b89c8b4c054346ca7febb189f8e6
Mark Chandler <mark.allen.chandler@gmail.com>
2017-04-12 18:36:41 -0400

    Add Provider to TeamForm

--- src/components/teams/TeamForm/stories.js
+++ src/components/teams/TeamForm/stories.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { storiesOf } from '@kadira/storybook'
+import Provider from 'provider'
 import { TeamForm } from 'components'
 
 storiesOf('TeamForm', module)
+  .addDecorator(Provider)
   .add('renders', () => (
-    <TeamForm>Render</TeamForm>
+    <TeamForm />
   ))
