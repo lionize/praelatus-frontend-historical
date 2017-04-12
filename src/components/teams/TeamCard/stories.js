da3aa1077c216afe1f6a4227847a8f62f9346316
Mark Chandler <mark.allen.chandler@gmail.com>
2017-04-12 18:36:11 -0400

    Add Provider to TeamCard

--- src/components/teams/TeamCard/stories.js
+++ src/components/teams/TeamCard/stories.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import { storiesOf } from '@kadira/storybook'
+import Provider from 'provider'
 import { TeamCard } from 'components'
 
+const team = {
+  id: 0,
+  name: 'Team 0',
+}
+
 storiesOf('TeamCard', module)
+  .addDecorator(Provider)
   .add('renders', () => (
-    <TeamCard>Render</TeamCard>
+    <TeamCard team={team} />
   ))
