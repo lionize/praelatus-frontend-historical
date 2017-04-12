5c4c7f5d751c35c38b0c74689eb9b3635838b3b8
Mark Chandler <mark.allen.chandler@gmail.com>
2017-04-12 18:39:33 -0400

    Add Provider to TicketCard

--- src/components/tickets/TicketCard/stories.js
+++ src/components/tickets/TicketCard/stories.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import { storiesOf } from '@kadira/storybook'
+import Provider from 'provider'
 import { TicketCard } from 'components'
 
+const ticket = {
+  id: 0,
+  key: 'TICKET-0',
+  summary: 'Ticket Summary',
+  description: 'Ticket Description'
+}
+
 storiesOf('TicketCard', module)
+  .addDecorator(Provider)
   .add('renders', () => (
-    <TicketCard>Render</TicketCard>
+    <TicketCard ticket={ticket} />
   ))
