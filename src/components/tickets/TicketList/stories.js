ce16b3d3f189d7381f2a86528148bca41e02da68
Mark Chandler <mark.allen.chandler@gmail.com>
2017-04-12 18:41:27 -0400

    Add Provider to TicketList

--- src/components/tickets/TicketList/stories.js
+++ src/components/tickets/TicketList/stories.js
@@ -1,8 +1,28 @@
 import React from 'react'
 import { storiesOf } from '@kadira/storybook'
+import Provider from 'provider'
 import { TicketList } from 'components'
 
+const store = {
+  data: {
+    tickets: {
+      keys: ['TICKET-0'],
+      byKey: {
+        'TICKET-0': {
+          id: 0,
+          key: 'TICKET-0',
+          summary: 'Ticket Summary',
+          description: 'Ticket Description'
+        }
+      }
+    }
+  }
+}
+
 storiesOf('TicketList', module)
+  .addDecorator(story => (
+    Provider(story, store)
+  ))
   .add('renders', () => (
     <TicketList>Render</TicketList>
   ))
