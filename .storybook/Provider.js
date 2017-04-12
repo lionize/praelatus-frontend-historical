56c8d046d0169256d3dbafdbe56dd76fc2a21195
Mark Chandler <mark.allen.chandler@gmail.com>
2017-04-12 16:31:32 -0400

    Add optional store argument to storybook Provider

--- .storybook/Provider.js
+++ .storybook/Provider.js
@@ -2,9 +2,9 @@ import React from 'react'
 import configureStore from 'redux-mock-store'
 import { Provider as ReduxProvider } from 'react-redux'
 
-let store = configureStore()({})
-
-export default function Provider(story) {
+export default function Provider(story, storeProps = {}) {
+  let store = configureStore()(storeProps)
+  
   return (
     <ReduxProvider store={store}>
       {story()}
