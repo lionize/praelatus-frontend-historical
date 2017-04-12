d36e493fc039d039fd4e8384f8647a1db758a241
Mark Chandler <mark.allen.chandler@gmail.com>
2017-04-12 18:42:19 -0400

    Add user prop to UserCard

--- src/components/users/UserCard/stories.js
+++ src/components/users/UserCard/stories.js
@@ -2,7 +2,14 @@ import React from 'react'
 import { storiesOf } from '@kadira/storybook'
 import { UserCard } from 'components'
 
+const user = {
+  id: 0,
+  username: 'user',
+  email: 'user@users.com',
+  fullName: 'User'
+}
+
 storiesOf('UserCard', module)
   .add('renders', () => (
-    <UserCard>Render</UserCard>
+    <UserCard user={user} />
   ))
