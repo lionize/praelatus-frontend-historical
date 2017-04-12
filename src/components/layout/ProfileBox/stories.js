46e7766404552e7e92a01d9183717a84ca2e566d
Mark Chandler <mark.allen.chandler@gmail.com>
2017-04-12 17:01:36 -0400

    Add Provider to ProfileBox

--- src/components/layout/ProfileBox/stories.js
+++ src/components/layout/ProfileBox/stories.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { storiesOf } from '@kadira/storybook'
+import Provider from 'provider'
 import { ProfileBox } from 'components'
 
 storiesOf('ProfileBox', module)
+  .addDecorator(Provider)
   .add('renders', () => (
-    <ProfileBox>Render</ProfileBox>
+    <ProfileBox user={{ username: 'mark' }} />
   ))
