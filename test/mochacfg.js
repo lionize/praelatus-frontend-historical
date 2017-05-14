3a5e4bce561eed89e165d06c737cdd05a14c9b80
Mark Chandler <mark.allen.chandler@gmail.com>
2017-05-13 23:33:20 -0400

    Remove mocha test configs

--- test/mochacfg.js
+++
@@ -1,8 +0,0 @@
-/*
-  Configuration file for .png extensions. Allows mocha to understand how to
-  handle the file format in component tests.
-*/
-
-require.extensions['.png'] = function mochaPngConfig() {
-  return null;
-};
